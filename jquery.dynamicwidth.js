2be214850c0f2ca0d9b123123957384fd3d2f850
Samuel Liew <samliew@gmail.com>
2018-07-03 10:01:59 +1000

    Create jquery.dynamicwidth.js

---
+++ jquery.dynamicwidth.js
@@ -0,0 +1,27 @@
+/*!
+ * jQuery Dynamic Width Plugin v1.0
+ * https://github.com/samliew/dynamic-width
+ *
+ * Copyright https://github.com/samliew
+ * Released under the MIT license
+ */
+ 
+(function($) {
+
+    $.fn.dynamicWidth = function() {
+        var plugin = $.fn.dynamicWidth;
+        if (!plugin.fakeEl) plugin.fakeEl = $('<span>').hide().appendTo(document.body);
+
+        function sizeToContent(el) {
+            var $el = $(el);
+            plugin.fakeEl.text(el.value || el.innerText || el.placeholder).css('font', $el.css('font'));
+            $el.css('width', plugin.fakeEl.width());
+        }
+
+        return this.each(function(i, el) {
+            sizeToContent(el);
+            $(el).on('change keypress keyup blur', evt => sizeToContent(evt.target));
+        });
+    };
+
+})(jQuery);
