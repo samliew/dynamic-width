1bc6ac705a9313980fe1e7b3cf167f138f1353fe
Samuel Liew <samliew@gmail.com>
2023-04-04 10:15:05 +0800

    Update jquery.dynamicWidth.js
    minor changes

--- jquery.dynamicWidth.js
+++ jquery.dynamicWidth.js
@@ -10,16 +10,27 @@
 
     const params = {
          minWidth: 0,
-         additionalPadding: 5
+         additionalPadding: 3
     };
 
+    /**
+     * @summary Dynamically resize element to width of text content
+     * @returns {jQuery} jQuery object (chainable)
+     * @link https://github.com/samliew/dynamic-width
+     * @example
+     *   $('input').dynamicWidth();
+     */
     $.fn.dynamicWidth = function (opts) {
         $.extend(params, opts);
 
         const plugin = $.fn.dynamicWidth;
-        if (!plugin.fakeEl) plugin.fakeEl = $('<span style="position:absolute;"></span>').hide().appendTo(document.body);
 
-        function sizeToContent (el) {
+        // Reusable private hidden element to measure text width
+        if (!plugin.fakeEl) {
+            plugin.fakeEl = $('<span style="position:absolute; display:none;"></span>').appendTo(document.body);
+        }
+
+        const sizeToContent = el => {
             const $el = $(el);
             const cs = getComputedStyle(el);
             plugin.fakeEl.text(el.value || el.innerText || el.placeholder)
@@ -31,11 +42,12 @@
             const elemPadding = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight);
             const newWidth = plugin.fakeEl.width() + elemPadding + params.additionalPadding;
             $el.css('width', newWidth > params.minWidth ? newWidth : params.minWidth);
-        }
+        };
 
         return this.each(function (i, el) {
             sizeToContent(el);
-            $(el).on('change keypress keyup blur', evt => sizeToContent(evt.target));
+            $(el).off('change keypress keyup blur')
+              .on('change keypress keyup blur', evt => sizeToContent(evt.target));
         });
     };
 
